ac0f5cce5b50368985eb5ff4296edafdf485355d
Leonardo Citton <>
2023-02-15 10:27:12 +0100

    refactoring with the ui cartel

--- components/PasswordModal.js
+++ components/PasswordModal.js
@@ -1,4 +1,4 @@
-import Button from './Button'
+import Button from '@/components/ui/Button'
 import { useRef, useState } from 'react'
 import { motion } from 'framer-motion'
 import { securityState } from '@/store/atom/securityAtom'
