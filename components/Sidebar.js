0226497f2234f85817fb8e9d299bbc6c7b13c829
Leonardo Citton <>
2023-02-16 10:13:28 +0100

    refactoring and animation to sidebar buttons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,7 @@
+import { useState } from "react"
 import { menuState } from "@/store/atom/sidebarAtom"
 import { useRecoilValue, useSetRecoilState } from 'recoil'
+import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion"
 import KeyTag from './ui/KeyTag'
 import IconCartel from '../public/icon/icon-cartel.svg'
 import IconFolder from '../public/icon/icon-folder.svg'
@@ -9,6 +11,9 @@ import IconPhone from '../public/icon/icon-phone.svg'
 export default function Sidebar() {
   const setMenu = useSetRecoilState(menuState)
   const menu = useRecoilValue(menuState)
+
+  const [buttonArray] = useState(['Time Planner', 'Assets', 'Appuntamenti', 'Contatti'])
+  const [hovered, setHovered] = useState(buttonArray[0])
 
   return (
     <div className='w-[300px] fixed top-0 bottom-0 bg-white shadow-sidebar flex flex-col justify-between pt-10 pb-6'>
@@ -26,69 +31,47 @@ export default function Sidebar() {
           <div className='flex flex-col gap-y-4 mx-6'>
             {/* <p className='text-sm uppercase text-slate-500 tracking-[1px]'> menù </p> */}
 
-            <div className='flex flex-col gap-y-[6px]'>
-              <button
-                className={`button-sidebar ${menu == 1 ? 'button-sidebar-active' : ''}`}
-                onClick={() => setMenu(1)}
-              >
-                <div className='flex items-center gap-x-[6px] py-[2px]'>
-                  <span className='w-5 h-5'>
-                    <IconCartel />
-                  </span>
-                  <p className="text-base font-medium"> Time Planner </p>
-                </div>
-                <KeyTag
-                  text="⌘1" 
-                  color={`${menu == 1 ? 'white' : 'slate-900'}`} 
-                />
-              </button>
-              <button
-                className={`button-sidebar ${menu == 2 ? 'button-sidebar-active' : ''}`}
-                onClick={() => setMenu(2)}
-              >
-                <div className='flex items-center gap-x-[6px] py-[2px]'>
-                  <span className='w-5 h-5'>
-                    <IconFolder />
-                  </span>
-                  <p className="text-base font-medium"> Assets </p>
-                  <p className="text-base text-slate-500/50"> (12) </p>
-                </div>
-                <KeyTag
-                  text="⌘2" 
-                  color={`${menu == 2 ? 'white' : 'slate-900'}`} 
-                />
-              </button>
-              <button
-                className={`button-sidebar ${menu == 3 ? 'button-sidebar-active' : ''}`}
-                onClick={() => setMenu(3)}
-              >
-                <div className='flex items-center gap-x-[6px] py-[2px]'>
-                  <span className='w-5 h-5'>
-                    <IconCalendar />
-                  </span>
-                  <p className="text-base font-medium"> Appuntamenti </p>
-                </div>
-                <KeyTag
-                  text="⌘3" 
-                  color={`${menu == 3 ? 'white' : 'slate-900'}`} 
-                />
-              </button>
-              <button
-                className={`button-sidebar ${menu == 4 ? 'button-sidebar-active' : ''}`}
-                onClick={() => setMenu(4)}
-              >
-                <div className='flex items-center gap-x-[6px] py-[2px]'>
-                  <span className='w-5 h-5'>
-                    <IconPhone />
-                  </span>
-                  <p className="text-base font-medium"> Contatti </p>
-                </div>
-                <KeyTag
-                  text="⌘4" 
-                  color={`${menu == 4 ? 'white' : 'slate-900'}`} 
-                />
-              </button>
-            </div>
+            <AnimatePresence>
+              <div onMouseLeave={() => setHovered(buttonArray[menu-1])} className='flex flex-col gap-y-[6px]'>
+                <AnimateSharedLayout crossfade>
+                  {buttonArray.map((text, index) => (
+                    <button
+                      key={text}
+                      className={`button-sidebar ${menu == (index+1) ? 'button-sidebar-active' : ''} relative`}
+                      onClick={() => setMenu((index+1))}
+                      onMouseEnter={() => setHovered(text)}
+                    >
+                      <div className='flex items-center gap-x-[6px] py-[2px]'>
+                        <span className='w-5 h-5'>
+                          { index == 0 && <IconCartel /> }
+                          { index == 1 && <IconFolder /> }
+                          { index == 2 && <IconCalendar /> }
+                          { index == 3 && <IconPhone /> }
+                        </span>
+                        <p className='text-base font-medium'> { text } </p>
+                        {text === 'Assets' && <p className='text-base opacity-80'>(12)</p>}
+                      </div>
+                      <KeyTag
+                        text={`⌘${index + 1}`}
+                        color={`${menu == (index+1) ? 'white' : 'slate-900'}`} 
+                      />
+
+                      {/* background framer animation */}
+                      <AnimatePresence>
+                        {hovered === text && (
+                          <motion.div
+                            layoutId='sidebar-button-hover-background'
+                            className='sidebar-button-hover-background z-[-1]'
+                            initial={false}
+                            transition={{sidebarButtonsTransition}}
+                          />
+                        )}
+                      </AnimatePresence>
+                    </button>
+                  ))}
+                </AnimateSharedLayout>
+              </div>
+            </AnimatePresence>
           </div>
         </div>
 
@@ -98,4 +81,8 @@ export default function Sidebar() {
         </div>
     </div>
   )
+}
+
+const sidebarButtonsTransition = {
+  ease: 'circOut',
 }
